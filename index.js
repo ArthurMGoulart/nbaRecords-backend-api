55afd0936f4a0b67d01dd2d8823b4d01be21fff8
ArthurMGoulart <budiarthur@gmail.com>
2022-03-10 13:20:51 -0300

    style: change message on home

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use('/login', loginRouter);
 
 app.use('/player', playerRouter);
 
-app.get('/', (req, res) => res.status(200).json({ message: `Running Server at ${port}` }));
+app.get('/', (req, res) => res.status(200).json({ message: 'Running Server Nba Records' }));
 
 app.listen(port, () => console.log(`Servidor online na porta ${port}`));
