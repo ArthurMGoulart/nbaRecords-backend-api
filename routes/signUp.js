7e39f37eccb0269b1e7bccc3dd70de17afaa6438
ArthurMGoulart <budiarthur@gmail.com>
2022-03-30 10:29:50 -0300

    fix: signUpSchema

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { validateWithJoy } = require('../middlewares/validateWithJoi');
-const { signupSchema } = require('../schemas/schemasJoi');
+const { signUpSchema } = require('../schemas/schemasJoi');
 const userController = require('../controllers/user');
 
 const signUpRouter = express.Router();
 
-signUpRouter.post('/', validateWithJoy(signupSchema), userController.signUp);
+signUpRouter.post('/', validateWithJoy(signUpSchema), userController.signUp);
 
 module.exports = {
   signUpRouter,
